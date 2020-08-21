4015d9d5a49956f7c9e87317bdfc4927f0cff8da
nnabdou <nnabdou@gmail.com>
2020-08-20 20:43:27 -0400

    version 8

--- store.js
+++ store.js
@@ -1,21 +1,37 @@
-var removeCartItemButtons = document.getElementsByClassName('btn-danger')
-for (var i = 0; i < removeCartItemButtons.length; i++) {
-    var button = removeCartItemButtons[i]
-    button.addEventListener('click', function(event) {
-        var buttonClicked = event.target
-        buttonClicked.parentElement.parentElement.remove()
-        updateCartTotal()
-    
-    })
+if (document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', ready)
+} else {
+    ready()
 }
+
+function ready() {
+    var removeCartItemButtons = document.getElementsByClassName('btn-danger')
+    for (var i = 0; i < removeCartItemButtons.length; i++) {
+        var button = removeCartItemButtons[i]
+        button.addEventListener('click', removeCartItem)
+    }
+}
+
+
+function removeCartItem(event) [
+    var buttonClicked = event.target
+    buttonClicked.parentElement.parentElement.remove()
+    updateCartTotal()
+]
+
 
 function updateCartTotal() {
     var cartItemContainer = document.getElementsByClassName('cart-items')[0]
     var cartRows = cartItemContainer.getElementsByClassName('cart-row')
+    var total = 0
     for (var i = 0; i < cartRows.length; i++) {
         var cartRow = cartRows[i]
         var priceElement = cartRow.getElementsByClassName('cart-price')[0]
         var quantityElement = cartRow.getElementsByClassName('cart-quanitity-input')[0]
-        
+        var price = parseFloat(priceElement.innerText.replace('$', ''))
+        var quantity = quantityElement.nodeValue
+        total = total + (price * quantity)
     }
+    document.getElementsByClassName('cart-total-price')[0].innerText = '$' + total
 }
+
